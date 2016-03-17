d3a9e7915fc5f67cd2fde52ace249a19fa94ebfb
duanhongyi <duanhongyi@doopai.com>
2016-03-17 20:19:24 +0800

    init repo

---
+++ mkdocs_extensions/__init__.py
@@ -0,0 +1,3 @@
+from slugify import slugify_unicode
+
+ slugify = lambda x, y: slugify_unicode(x)
---
+++ setup.py
@@ -0,0 +1,33 @@
+# coding=utf8
+
+from setuptools import setup, find_packages
+
+
+setup(
+    name='mkdocs-extensions',
+    version='0.1.1',
+
+    author='Duan Hongyi',
+    author_email='duanhongyi@doopai.com',
+
+    url='https://github.com/duanhongyi/mkdocs-slugify',
+    description='Python mkdocs extensions',
+
+    packages=find_packages(),
+    install_requires=[
+        'awesome-slugify',
+    ],
+
+    license='GNU GPLv3',
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+    keywords='slugify slug transliteration russian german unicode translation flexible',
+)
